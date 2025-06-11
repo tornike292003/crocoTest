17d1768f37a7aedec365e2915fe6c80af23b8086
Toko0829 <tokomeshvildishvili6@gmail.com>
2025-06-11 16:01:42 +0400

    added search bar

--- src/app/core/table/table.component.ts
+++ src/app/core/table/table.component.ts
@@ -1,11 +1,12 @@
 import { Component, inject } from '@angular/core';
 import { MasterService } from '../../master.service';
 import { HeaderComponent } from "../header/header.component";
-import { Router, RouterLink } from '@angular/router';
+import { Router } from '@angular/router';
+import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms'
 
 @Component({
   selector: 'app-table',
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, FormsModule, ReactiveFormsModule],
   templateUrl: './table.component.html',
   styleUrl: './table.component.scss'
 })
@@ -14,17 +15,30 @@ export class TableComponent {
   usersData: any[] = [];
   masterService = inject(MasterService);
   router = inject(Router)
+  filteredData: any[] = [];
+  searchUser = new FormControl('')
 
   ngOnInit(){
     this.masterService.getUsers().subscribe((data) => {
       this.usersData = data;
-      console.log(data);
-    })
+      this.filteredData = data;
+    });
+
+    this.searchUser.valueChanges.subscribe(searchText => {
+      const text = searchText?.toLowerCase() || '';
+      this.filteredData = this.usersData.filter(user => 
+        user.name.toLowerCase().includes(text) || user.email.toLowerCase().includes(text)
+      );
+    });
+
   }
 
   goToUserPosts(id:number){
     this.router.navigate(['/posts', id])
   }
+
+
+
 
 
 }
